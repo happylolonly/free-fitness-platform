410fcbeb4b4e9400a4b16e6aa6cde75023017b86
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-02-07 12:37:46 +0700

    remove useMongoClient param

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,18 +1,15 @@
 import config from '../configs/config.env';
 
-
 export default (mongoose, callback) => {
-
   mongoose.Promise = global.Promise;
 
-  mongoose.connect(config.db.url, { useMongoClient: true });
+  mongoose.connect(config.db.url);
   mongoose.connection
-     .once('open', () => {
-       console.log('ready')
+    .once('open', () => {
+      console.log('ready');
       //  callback();
-     })
-     .on('error', (error) => {
-       console.log('Db connect error: \n', error);
-     });
-
+    })
+    .on('error', error => {
+      console.log('Db connect error: \n', error);
+    });
 };
